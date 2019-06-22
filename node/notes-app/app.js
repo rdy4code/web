0e55f8aa20ed06875ab73a9d876f93cf7ef2dfc9
Sergey Uskov <rdy4code@gmail.com>
2019-06-22 13:08:15 +0300

    importing own files

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,3 +1,10 @@
-const fs = require("fs")
-fs.writeFileSync("notes.txt", "This file was created with nodejs.")
-fs.appendFileSync("notes.txt", " I live in Moscow.")
+// const fs = require("fs")
+// fs.writeFileSync("notes.txt", "This file was created with nodejs.")
+// fs.appendFileSync("notes.txt", " I live in Moscow.")
+require("./utils.js")
+
+
+const name = "Andrew";
+
+console.log("--> " + name);
+
