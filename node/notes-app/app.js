b63b8602ba85db709eeedd880ea99a53d51f48e5
Sergey Uskov <rdy4code@gmail.com>
2019-06-23 12:02:58 +0300

    test comit

--- node/notes-app/app.js
+++ node/notes-app/app.js
@@ -1,10 +1,12 @@
 // const fs = require("fs")
 // fs.writeFileSync("notes.txt", "This file was created with nodejs.")
 // fs.appendFileSync("notes.txt", " I live in Moscow.")
-require("./utils.js")
+// const add = require("./utils.js")
+// const sum = add(3, 4);
+// console.log(sum);
 
-
-const name = "Andrew";
-
-console.log("--> " + name);
+const getNotes = require('./nodes.js');
+const msg = getNotes();
+console.log(msg);
+console.log("test");
 
--- node/notes-app/utils.js
+++ node/notes-app/utils.js
@@ -1,1 +1,9 @@
 console.log("utils.js");
+
+const name = "Mike";
+
+const add = function(a, b){
+    return a + b;
+}
+
+module.exports = add
