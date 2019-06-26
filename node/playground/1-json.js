78d185d760b9a525594211448262751b4252bd35
Sergey Uskov <rdy4code@gmail.com>
2019-06-26 13:25:55 +0300

    write and read JSON data from a file

--- node/playground/1-json.js
+++ node/playground/1-json.js
@@ -1,10 +1,30 @@
- const book = {
-     title: "Ego is an enemy",
-     author: "Ryan Holiday"
- }
+const fs = require("fs"); 
+
+/* Create JSON file for an object */
+
+const book = {
+    title: "Ego is an enemy",
+    author: "Ryan Holiday"
+}
 
 const bookJSON = JSON.stringify(book);
-console.log(bookJSON);
+fs.writeFileSync("1-json.json", bookJSON)
 
-const bookParsed = JSON.parse(bookJSON);
-console.log(bookParsed.author);
+// - - - - - 
+
+/* Read JSON file (need convert binary code to string) */
+
+const dataBuffer = fs.readFileSync("1-json.json")\
+console.log(dataBuffer); //Binary code
+const dataJSON = dataBuffer.toString(); // convert data to a standard string
+console.log(dataJSON)
+
+const data = JSON.parse(dataJSON); // parsing to an object
+console.log(data.title) //Access to the object's title
+
+// - - - - - 
+
+
+// console.log(bookJSON);
+// const bookParsed = JSON.parse(bookJSON);
+// console.log(bookParsed.author);
