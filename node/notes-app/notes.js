4fec3ae631a324e017d3e02ad195ade23edd8f8b
Sergey Uskov <rdy4code@gmail.com>
2019-07-05 01:24:50 +0300

    check for duplicates

--- node/notes-app/notes.js
+++ node/notes-app/notes.js
@@ -5,19 +5,41 @@ const getNotes = () =>{
 }
 
 const addNote = (title, body) =>{
-    const notes = loadNotes()
-    console.log(notes);
+    const notes = loadNotes();
+
+    //Check for duplicates
+    const duplicateNotes = notes.filter(function(note) {
+        return note.title === title
+
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log("New note added!");
+    } else {
+        console.log("Note title taken!");
+    }
 }
+
 // Read--> toString --> parse
 const loadNotes = () => {
     try {
-        const notes = loadNotes()
-        const dataBuffer = fs.readFileSync("notes.json") //Read file /than
-        const dataJSON = dataBuffer.toString()
-        return JSON.parse(dataJSON)
+        const dataBuffer = fs.readFileSync("notes.json") //Read file -->
+        const dataJSON = dataBuffer.toString()           //toString  -->
+        return JSON.parse(dataJSON)                      //parse
     } catch (err) {
         return []
     }
+}
+
+const saveNotes = (notes) =>{
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync("notes.json", dataJSON);
+
 }
 
 module.exports = {
