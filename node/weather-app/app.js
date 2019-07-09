012cfa5485bff07ea40f7068fb3bf5190ccb7e69
Sergey Uskov <rdy4code@gmail.com>
2019-07-09 17:36:15 +0300

    dynamic geocoding request

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require("request"); //easy http calls
-
-const url =
-  "https://api.darksky.net/forecast/6545fe1a18ea817905dd916d66442ff1/55.751244, -122?units=si"; //from darksky API
+const geocode = require("./utils/geocode");
+// const url =
+// "https://api.darksky.net/forecast/6545fe1a18ea817905dd916d66442ff1/55.751244, -122?units=si"; //from darksky API
 // ,37.618423?units=si
 
 //Request ({object obligatory}, callback function with 2 arguments (error - if something went wrong)
@@ -25,8 +25,6 @@ const url =
 //   }
 // });
 
-// });
-
 // Geocoding
 // Address --> convert to Lat/Long --> make request
 // const mapUrl =
@@ -36,23 +34,9 @@ const url =
 //   "pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A" + //given public token
 //   "&limit=1"; //option for quantity
 
-const mapUrl =
-  "https://api.mapbox.com/geocoding/v5/mapbox.places/moscow.json?access_token=pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A&limit=1";
+/* * Dynamic url request * */
 
-request({ url: mapUrl, json: true }, (error, response) => {
-  if (error) {
-    console.log("Unable to connect to map data service!");
-  } else if (response.body.features.length === 0) {
-    console.log("Unable to find such city name. Thy another search!");
-  } else {
-    const latitude = response.body.features[0].center[0];
-    const longitude = response.body.features[0].center[1];
-    console.log(latitude, longitude);
-  }
-
-  // console.log(response);
+geocode("Saint-Peterburg", (error, data) => {
+  console.log("Error", error);
+  console.log("Data", data);
 });
-
-// request((url: url2, json: true),(error, response) => {})
-
-//pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A
---
+++ node/weather-app/utils/geocode.js
@@ -0,0 +1,24 @@
+const request = require("request");
+
+const geocode = (address, callback) => {
+  const url =
+    "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+    address +
+    ".json?access_token=pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A&limit=1";
+
+  request({ url: url, json: true }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to geolocation service", undefined);
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location. Try another search.", undefined);
+    } else {
+      callback(undefined, {
+        latitude: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode;
