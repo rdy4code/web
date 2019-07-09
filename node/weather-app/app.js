0c5fe7dbb88c90d1028fb4f9adb51337591aba5c
Sergey Uskov <rdy4code@gmail.com>
2019-07-09 12:15:25 +0300

    latitude/longitude  from json mapdata

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -1,19 +1,45 @@
 const request = require("request"); //easy http calls
 
 const url =
-  "https://api.darksky.net/forecast/6545fe1a18ea817905dd916d66442ff1/37.8267,-122.4233"; //from darksky API
+  "https://api.darksky.net/forecast/6545fe1a18ea817905dd916d66442ff1/55.751244,37.618423?units=si"; //from darksky API
 
 //Request ({object obligatory}, callback function with 2 arguments (error - if something went wrong)
 //and (response - if everything is OK)
 
-request({ url: url, json: true }, (error, response) => {
-  // console.log(response.body.currently);
+// request({ url: url, json: true }, (error, response) => {
+//   // console.log(response.body.currently);
 
-  console.log(
-    "It is currently " +
-      response.body.currently.temperature +
-      " degrees out. There is a " +
-      response.body.currently.precipProbability +
-      "% chance of rain."
-  );
-});
+//   console.log(
+//     "It is currently " +
+//     response.body.currently.temperature +
+//     " degrees out. There is a " +
+//     response.body.currently.precipProbability +
+//     "% chance of " +
+//     response.body.daily.data[0].precipType + //array bracket notation for 0 element
+//       "."
+//   );
+// });
+
+// Geocoding
+// Address --> convert to Lat/Long --> make request
+const mapUrl =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
+  "Los%20Angeles.json" + //String city request
+  "?access_token=" +
+  "pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A" + //given public token
+  "&limit=1"; //option for quantity
+
+const mapUrlF =
+  "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A&limit=1";
+
+// request({ url: mapUrl, json: true }, (error, response) => {
+//   const latitude = response.body.features[0].center[0];
+//   const longitude = response.body.features[0].center[1];
+
+//   // console.log(response);
+//   console.log(latitude, longitude);
+// });
+
+// request((url: url2, json: true),(error, response) => {})
+
+//pk.eyJ1IjoicmR5NGNvZGUiLCJhIjoiY2p4dXRtaXc3MTR0MTNibzFtZnE2Mzh6NiJ9.-jQONiy1YgtJERYE3k723A
