17da61d5e67234229a98fb8988f20badc999f209
Sergey Uskov <rdy4code@gmail.com>
2019-07-10 11:00:26 +0300

    geolocation + weather chain request

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -5,24 +5,15 @@ const forecast = require("./utils/forecast");
 //Request ({object obligatory}, callback function with 2 arguments (error - if something went wrong)
 //and (response - if everything is OK)
 
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to weather service!");
-//   } else if (response.body.error) {
-//     console.log("Unable to find location");
-//   } else {
+/* * Dynamic geolocation + weather chaining request * */
 
-//   }
-// });
-
-/* * Dynamic geolocation url request * */
-
-// geocode("Saint-Peterburg", (error, data) => {
-//   console.log("Error", error);
-//   console.log("Data", data);
-// });
-
-forecast(-75.7088, 44.1545, (error, data) => {
+geocode("Saint-Peterburg", (error, data) => {
   console.log("Error", error);
   console.log("Data", data);
+
+  // data.lat/long recieved from geocode because of chaining
+  forecast(data.latitude, data.longitude, (error, data) => {
+    console.log("Error", error);
+    console.log("Data", data);
+  });
 });
--- node/weather-app/utils/geocode.js
+++ node/weather-app/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const geocode = (latitude, longitude, callback) => {
+const geocode = (address, callback) => {
   const url =
     "https://api.mapbox.com/geocoding/v5/mapbox.places/" +
     address +
@@ -13,9 +13,9 @@ const geocode = (latitude, longitude, callback) => {
       callback("Unable to find location. Try another search.", undefined);
     } else {
       callback(undefined, {
-        latitude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
+        location: response.body.features[0].place_name,
+        latitude: response.body.features[0].center[1],
+        longitude: response.body.features[0].center[0]
       });
     }
   });
