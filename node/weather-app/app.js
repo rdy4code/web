c3636e9efd1cde510a8dcfe0ebde635c07b9c269
Sergey Uskov <rdy4code@gmail.com>
2019-07-10 11:49:13 +0300

    terminal enter geolocation

--- node/weather-app/app.js
+++ node/weather-app/app.js
@@ -2,18 +2,29 @@ const request = require("request"); //easy http calls
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-//Request ({object obligatory}, callback function with 2 arguments (error - if something went wrong)
-//and (response - if everything is OK)
+const address = process.argv[2]; // input from the console
+
+if (!address) {
+  return console.log("Please provide an address!");
+}
 
 /* * Dynamic geolocation + weather chaining request * */
 
-geocode("Saint-Peterburg", (error, data) => {
+geocode(address, (error, data) => {
+  if (error) {
+    return console.log(error); //after RETURN statement --> function stops and no need to use ELSE statement
+  }
   console.log("Error", error);
   console.log("Data", data);
 
   // data.lat/long recieved from geocode because of chaining
-  forecast(data.latitude, data.longitude, (error, data) => {
-    console.log("Error", error);
-    console.log("Data", data);
+  forecast(data.latitude, data.longitude, (error, forecastData) => {
+    if (error) {
+      return console.log(error); //after RETURN statement --> function stops and no need to use ELSE statement
+    }
+    console.log(data.location);
+    console.log(forecastData);
+    // console.log("Error", error);
+    // console.log("Data", forecastData);
   });
 });
