9519d1f36601145875ae3c4c54ce9d0d1b2e475b
Sergey Uskov <rdy4code@gmail.com>
2019-07-11 14:15:49 +0300

    previous scratch to continue

--- node/server/src/app.js
+++ node/server/src/app.js
@@ -1,26 +1,20 @@
+const path = require("path")
 const express = require("express")
 
 const app = express(); 
 
+const publicDirectoryPath = path.join(__dirname, "../public"); //to reroute static file
+app.use(express.static(publicDirectoryPath))
+
 //app.com
 
-app.get("", (req, res) => {
-    res.send("Hello express!")    
-})
-
-app.get("/help", (req, res) => {
-    res.send("Help page")    
-})
-app.get("/about", (req, res) => {
-    res.send("About page")    
-})
-app.get("/weather", (req, res) => {
-    res.send("Your weather information")    
-})
+// app.get("/about", (req, res) => {
+//     res.send("About page")    
+// })
 
 app.listen(3000, () => {
     console.log("Server is runninng on port 3000");
 });
 
 //Ctrl+C to shutdowm the server
-//nodemon src/app.js 
+//nodemon src/app.js  
